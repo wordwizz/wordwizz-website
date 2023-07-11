9702854b63708f0ca3612ce47b9c4762bcd34f3d
Abhilash Cuberoot <abhilash@cuberoot.tech>
2023-07-11 23:18:16 +0530

    feat: add support for toast notifications

--- main.js
+++ main.js
@@ -3,6 +3,20 @@
 import Glide, { Autoplay, Controls } from '@glidejs/glide/dist/glide.modular.esm';
 import Typewriter from 'typewriter-effect/dist/core';
 
+/* Function to show toast notifications */
+const showNotification = function (message, isSuccess) {
+  const notificationElement = document.getElementById("notification");
+  notificationElement.textContent = message;
+  const classToAdd = isSuccess ? "notification-success" : "notification-warning";
+  notificationElement.classList.add(classToAdd);
+  notificationElement.classList.add('show');
+
+  // Hide notification after 3 seconds (adjust as needed)
+  setTimeout(function() {
+  notificationElement.classList.add('hide');
+  }, 3000);
+}
+
 const domReady = function() {
   /* Hamburger Menu */
   const nav_main = document.getElementById('main-nav')
