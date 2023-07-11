7bafe03be77ddaab5e6213e99698762b1e5516e5
Abhilash Cuberoot <abhilash@cuberoot.tech>
2023-07-11 22:04:18 +0530

    refactor: optimize main js script for performance

--- main.js
+++ main.js
@@ -1,71 +1,79 @@
-/* Smooth Scrolling behavior with fixed header offset */
-document.addEventListener('click', (event) => {
-  if (event.target.matches('a[href^="#"]')) {
-    const id = event.target.getAttribute('href').substring(1);
-    const element = document.getElementById(id);
-    if (element) {
-      const rootStyles = getComputedStyle(document.documentElement);
-      const headerHeightValue = rootStyles.getPropertyValue('--header-height');
+"use strict";
 
-      // Create a temporary element
-      const tempElement = document.createElement('div');
-      tempElement.style.setProperty('height', headerHeightValue);
-
-      document.body.appendChild(tempElement);
-      const computedStyle = getComputedStyle(tempElement);
-      const headerHeight = parseInt(computedStyle.getPropertyValue('height'));
-      document.body.removeChild(tempElement);
-
-      const top = element.getBoundingClientRect().top + window.pageYOffset - headerHeight;
-      window.scrollTo({ top, behavior: 'smooth' });
-      event.preventDefault();
-    }
-  }
-});
-
-/* Hamburger Menu */
-const nav_main = document.getElementById('main-nav')
-const nav_toggle = document.getElementById('nav-toggle')
-
-nav_toggle.addEventListener('click', function(){
-  if (nav_main.classList.contains('toggle-show')) {
-    nav_main.classList.remove('toggle-show')
-  } else {
-    nav_main.classList.add('toggle-show')
-  }
-});
-
-
-/* Slider */
-import Glide, { Autoplay, Controls } from '@glidejs/glide/dist/glide.modular.esm'
-const glider = new Glide('.glide', {
-  type: 'carousel',
-  perView: 1,
-  focusAt: 'center',
-  keyboard: true,
-  autoplay: 8000,
-  animationDuration: 1000
-})
-
-glider.mount({ Autoplay, Controls })
-
-
-
-/* Type Writer Effect */
+import Glide, { Autoplay, Controls } from '@glidejs/glide/dist/glide.modular.esm';
 import Typewriter from 'typewriter-effect/dist/core';
 
-setTimeout(() => {
-  const typewriter = new Typewriter('.typewriter', {
-    typeString: 'Your Brand<br/>Our Words',
-    loop: true,
-    delay: 75
+const domReady = function() {
+  /* Hamburger Menu */
+  const nav_main = document.getElementById('main-nav')
+  const nav_toggle = document.getElementById('nav-toggle')
+
+  nav_toggle.addEventListener('click', function(){
+    if (nav_main.classList.contains('toggle-show')) {
+      nav_main.classList.remove('toggle-show')
+    } else {
+      nav_main.classList.add('toggle-show')
+    }
   });
-  typewriter
-    .pauseFor(500)
-    .typeString('Your Brand<br/>Our Words')
-    .pauseFor(3000)
-    .deleteChars(24)
-    .typeString('Your Brand<br/>Our Words')
-    .pauseFor(2000)
-    .start();
-}, 3000)
+
+  /* Slider */
+  const glider = new Glide('.glide', {
+    type: 'carousel',
+    perView: 1,
+    focusAt: 'center',
+    keyboard: true,
+    autoplay: 8000,
+    animationDuration: 1000
+  })
+  glider.mount({ Autoplay, Controls })
+
+
+  /* Type Writer Effect */
+  setTimeout(() => {
+    const typewriter = new Typewriter('.typewriter', {
+      typeString: 'Your Brand<br/>Our Words',
+      loop: true,
+      delay: 75
+    });
+    typewriter
+      .pauseFor(500)
+      .typeString('Your Brand<br/>Our Words')
+      .pauseFor(3000)
+      .deleteChars(24)
+      .typeString('Your Brand<br/>Our Words')
+      .pauseFor(2000)
+      .start();
+  }, 3000)
+
+  /* Smooth Scrolling behavior with fixed header offset */
+  document.addEventListener('click', (event) => {
+    if (event.target.matches('a[href^="#"]')) {
+      const id = event.target.getAttribute('href').substring(1);
+      const element = document.getElementById(id);
+      if (element) {
+        const rootStyles = getComputedStyle(document.documentElement);
+        const headerHeightValue = rootStyles.getPropertyValue('--header-height');
+
+        // Create a temporary element
+        const tempElement = document.createElement('div');
+        tempElement.style.setProperty('height', headerHeightValue);
+
+        document.body.appendChild(tempElement);
+        const computedStyle = getComputedStyle(tempElement);
+        const headerHeight = parseInt(computedStyle.getPropertyValue('height'));
+        document.body.removeChild(tempElement);
+
+        const top = element.getBoundingClientRect().top + window.pageYOffset - headerHeight;
+        window.scrollTo({ top, behavior: 'smooth' });
+        event.preventDefault();
+      }
+    }
+  });
+};
+
+
+if (document.readyState === "complete" || (document.readyState !== "loading" && !document.documentElement.doScroll)) {
+  domReady();
+} else {
+  document.addEventListener("DOMContentLoaded", domReady);
+}
