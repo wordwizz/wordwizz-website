75e3b14d45bb83c0aa9f948bcf25ec7ae9ff87ad
Abhilash Cuberoot <abhilash@cuberoot.tech>
2023-07-16 18:34:46 +0530

    chore: remove webfont plugin from vite

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,10 @@
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-import ViteWebfontDownload from 'vite-plugin-webfont-dl';
 import Sitemap from 'vite-plugin-sitemap'
 import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default {
   plugins: [
     ViteImageOptimizer({}),
-    ViteWebfontDownload(
-      [],
-      {
-        injectAsStyleTag: true,
-        minifyCss: true,
-        async: true,
-        cache: true,
-        proxy: false,
-      }
-    ),
     Sitemap({
       hostname: "https://wordwizz.in/",
       exclude: ["/php"],
